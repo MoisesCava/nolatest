3e6293590bbe2dd51fb445163d98f9dd18edaeb0
MoisesCava <moiseslsth2204@gmail.com>
2023-04-06 10:20:51 -0400

    Creando servicios para las funciones de config

---
+++ frontend/client/src/services/profileService.js
@@ -0,0 +1,30 @@
+const getConfig = async () => {
+    try {
+        const response = await fetch('http://localhost:3000/config');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const updateConfig = async (username, photo) => {
+    try {
+        const response = await fetch('http://localhost:3000/config', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, photo })
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+  
+export {
+    getConfig,
+    updateConfig
+}
