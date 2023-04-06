0ba871b8680d92143e85e4e468476515a125cbb5
MoisesCava <moiseslsth2204@gmail.com>
2023-04-05 22:21:20 -0400

    Agregando go back para comportamiento responsive

--- frontend/client/src/components/chat/ChatDetail.jsx
+++ frontend/client/src/components/chat/ChatDetail.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { chatsToUse } from '../../contexts/ChatsContext';
 import AvatarProfile from '../profile/AvatarProfile';
 import { MdSearch } from "react-icons/md";
 import { HiDotsVertical } from 'react-icons/hi'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
 import IconButton from '../common/IconButton';
 import MessageSection from '../message/MessageSection';
+import useMobileVisibility from '../../hooks/useMobileVisibility';
+import { mobileBreakpoint } from '../../utils/customBreakPoints';
+import { showChatDetailToUse } from '../../contexts/MobileContext';
 
 const ChatDetail = () => {
+  const isMobile = useMobileVisibility(mobileBreakpoint);
+  const { saveChatClicked } = showChatDetailToUse();
   const { getCurrentChat } = chatsToUse();
   const current = getCurrentChat()
+
+  const handleSaveChatClicked = useCallback(
+    () => {
+      saveChatClicked(false)
+    },
+    [saveChatClicked]
+  );
   return (
     <div className="flex flex-col h-screen">
       <div className="flex justify-between bg-[#202d33] h-[65px] p-3">
@@ -16,6 +29,9 @@ const ChatDetail = () => {
           current.destinatary && 
           (
             <div className="flex items-center">
+              <div className={`${isMobile ? "hidden" : "sm:block"}`}>
+                <IconButton onDoClick={handleSaveChatClicked} icon={<AiOutlineArrowLeft />} />
+              </div>
               {/* Profile avatar */}
               <AvatarProfile destinatary={current.destinatary}/>
 
